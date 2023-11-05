6f79e4bdc6a9ba588c853e525bbf2e2705c6f125
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-04 23:52:52 -1200

    search movies api async to a variable and clg extract the data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import { useEffect } from "react";
 // 9333c6e3
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9333c6e3";
+
 const App = () => {
-  return <div className="App">APP</div>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return <h1 className="App">APP</h1>;
 };
 
 export default App;
