dac827d20ad82f88d8995c7e88449e79bf5fb16c
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-04 22:53:32 -1200

    Onclick prevCount  +1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+    </div>
+  );
+};
 
 export default App;
