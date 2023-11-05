914133ec971be9663ab9674637d4bc9bd8938dd9
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-05 00:24:50 -1200

    year in 1st movie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
-import SearchIcon from "./search.svg";
+// import SearchIcon from "./search.svg";
 import { useEffect } from "react";
 // 9333c6e3
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9333c6e3";
+
+const movie1 = {
+  Title: "Spiderman",
+  Year: "1990",
+  imdbID: "tt0100669",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -17,7 +25,40 @@ const App = () => {
     searchMovies("Spiderman");
   }, []);
 
-  return <h1 className="App">APP</h1>;
+  return (
+    <div className="app">
+      <h1>ShitFliix</h1>
+      <div className="search">
+        <input
+          placeholder="Search  For Movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="search-icon"
+          onClick={() => {}}
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>{" "}
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
